f26467c8af38ee81e8479fa33517203ade42ed02
Olivier <olivierlambertrouillard@gmail.com>
2023-09-26 21:54:26 -0400

    not working but let<s go for dict

---
+++ FlashCard/views/ThemeListView.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeListView.swift
+//  FlashCard
+//
+//  Created by Olivier Lambert Rouillard on 2023-09-11.
+//
+
+import SwiftUI
+
+struct ThemeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeListView()
+    }
+}
---
+++ FlashCard/views/buttons/TrashThemeView.swift
@@ -0,0 +1,20 @@
+//
+//  TrashThemeView.swift
+//  FlashCard
+//
+//  Created by Olivier Lambert Rouillard on 2023-09-11.
+//
+
+import SwiftUI
+
+struct TrashThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrashThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrashThemeView()
+    }
+}
